48b73f42a77c8982a7b911416b9af333c4c2b581
DanielSerio <dserio.d@gmail.com>
2025-05-22 17:38:14 -0400

    added base service methods

--- server/src/services/basic.service.ts
+++ server/src/services/basic.service.ts
@@ -1,30 +1,82 @@
-import { DataSource, EntityTarget, ObjectLiteral, Repository } from "typeorm";
+import { DataSource, EntityTarget, FindOptionsWhere, ObjectLiteral, Repository } from "typeorm";
 
-export abstract class BasicService<Entity extends ObjectLiteral, Create, Update> {
+export abstract class BasicService
+  <
+    Entity extends ObjectLiteral,
+    Create extends Partial<Entity>,
+    Update extends Partial<Entity>
+  > {
   protected get repo(): Repository<Entity> {
     return this.dataSource.getRepository(this.model);
   }
 
-  constructor(private model: EntityTarget<Entity>, protected dataSource: DataSource) { }
+  constructor(
+    private model: EntityTarget<Entity>,
+    protected dataSource: DataSource
+  ) { }
 
+  private getWhereFromCriteria(criteria: number | FindOptionsWhere<Entity>): FindOptionsWhere<Entity> {
+    let where = {};
 
-  create(createTaskCategoryDto: Create) {
-    return 'This action adds a new taskCategory';
+    if (typeof criteria === 'number') {
+      where = {
+        id: criteria
+      };
+    } else {
+      where = criteria;
+    }
+
+    return where;
   }
 
-  findAll() {
-    return `This action returns all taskCategories`;
+  create(create: Create) {
+    return this.repo.insert({
+      ...create
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} taskCategory`;
+  async findAll(params?: Parameters<typeof this.repo.find>[0]) {
+    const count = await this.repo.count(params?.where ? { where: params.where } : undefined);
+    const take = (params?.take ?? 25);
+    const skip = params?.skip ?? 0;
+    const pages = Math.ceil(count / take);
+
+    return {
+      records: await this.repo.find({
+        where: params?.where,
+        take,
+        skip,
+        order: params?.order,
+      }),
+      limit: take,
+      offset: skip,
+      select: params?.select ?? '*',
+      filter: params?.where ?? null,
+      sort: params?.order ?? null,
+      totals: {
+        records: count,
+        pages
+      }
+    };
   }
 
-  update(id: number, updateTaskCategoryDto: Update) {
-    return `This action updates a #${id} taskCategory`;
+  findOne(criteria: number | FindOptionsWhere<Entity>) {
+    const where = this.getWhereFromCriteria(criteria);
+
+    return this.repo.findOne({
+      where
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} taskCategory`;
+  update(criteria: number | FindOptionsWhere<Entity>, update: Update) {
+    const where = this.getWhereFromCriteria(criteria);
+
+    return this.repo.update(where, update);
+  }
+
+  remove(criteria: number | FindOptionsWhere<Entity>) {
+    const where = this.getWhereFromCriteria(criteria);
+
+    return this.repo.softDelete(where);
   }
 }
